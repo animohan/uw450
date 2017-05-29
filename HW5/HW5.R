f8df9e3486039095779d0a6f524a60569e95713e
anishmohan@gmail.com <anishmohan@gmail.com>
2017-05-29 07:49:19 -0700

    HW5 v3

--- HW5/HW5.R
+++ HW5/HW5.R
@@ -10,5 +10,30 @@ str(veh)
 
 
 library(e1071)
-svmfit = svm(CLASS ~., data = veh, kernel = "linear", cost = 10, scale = TRUE)
-plot(svmfit, veh, f0~f1)
+
+# Sample the data:
+set.seed(1)
+train = sample(nrow(veh),0.8*nrow(veh))
+test = -train
+
+veh.train = veh[train, ]
+veh.test = veh[test, ]
+
+
+set.seed(1)
+tuned.svm = tune(svm, CLASS ~., data = veh.train, kernel = "radial", ranges = list(cost = c(0.001,0.01,0.1,1,5,10,100)))
+summary(tuned.svm)
+best.model = tuned.svm$best.model
+best.model
+summary(best.model)
+#Training error
+svm.pred = predict(best.model, veh.train)
+table(predict = svm.pred, truth = veh.train$CLASS ) 
+
+
+#Test Error
+svm.test.pred = predict(best.model, veh.test)
+table(predict = svm.test.pred, truth = veh.test$CLASS )
+
+svmfit = svm(CLASS ~., data = veh, kernel = "radial", cost = 10, scale = TRUE)
+plot(svmfit, veh, f0~f7)
